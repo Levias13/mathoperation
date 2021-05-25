cdbf557a861e4c97cea1a9e1554d7fe35d3d21a6
DESKTOP-D06CVSI\Usuario <leslie.enriquez@bowpi.com>
2021-05-25 15:36:53 -0600

    comit inicial

---
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+package math.operation;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b)
+	{
+		return a+b;
+	}
+
+	public Integer sub(Integer a, Integer b)
+	{
+		return a-b;
+	}
+}
