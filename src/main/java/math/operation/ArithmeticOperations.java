e2acd2c218f11be2bf7e7f366ec6e00dcc620dc8
Leslie Enriquez Donado <leslie.enriquez13@gmail.com>
2021-06-16 14:08:35 -0600

    Update ArithmeticOperations.java

--- src/main/java/math/operation/ArithmeticOperations.java
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -11,17 +11,4 @@ public class ArithmeticOperations {
 	{
 		return a-b;
 	}
-
-	/*public Integer mul(Integer a, Integer b)
-	{
-		Integer result = a * b;
-		return result;
-	}
-
-	public Integer div(Integer a, Integer b)
-	{
-		return a/b;
-	}*/
-
-
 }
